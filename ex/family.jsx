3d70a883547f17fdc116d6544b36be73d2cf434c
thiagometal <thiagometal.fne@gmail.com>
2018-02-07 14:17:42 -0400

    propaga propriedade utilizando o operador spread

--- ex/family.jsx
+++ ex/family.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default props => (
     <div> 
-        {React.cloneElement(props.children, props)} 
+        {/* {React.cloneElement(props.children, props)}  */}
         {/* o props do componete filho vai ter o mesmo atributo que o props passado ao pai */}
+        {/* no exemplo a cima vc está passando diretamente a referencia da propriedade do pai para o filho */}
+        {/* o ideal é passar o clone usando o spread, como está a baixo */}
+        {React.cloneElement(props.children, {...props})}
     </div>
 )
