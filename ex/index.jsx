4a1b31089a4d4d3873a75db8bbea39be472dd518
thiagometal <thiagometal.fne@gmail.com>
2018-02-07 10:50:58 -0400

    cria primeiro componente personalizado

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Component from './component.jsx'
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+// ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDOM.render(<Component />, document.getElementById('app'))
